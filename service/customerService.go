849149f8d5dfe0e92eaed965b6cd7e0d4931600b
Manish Dangi <manish1998dangi@gmail.com>
2021-11-01 20:14:18 +0530

    Implemented GetCustomer API

--- service/customerService.go
+++ service/customerService.go
@@ -4,6 +4,7 @@ import "github.com/manishdangi98/banking/domain"
 
 type CustomerService interface {
 	GetAllCustomer() ([]domain.Customer, error)
+	GetCustomer(string) (*domain.Customer, error)
 }
 type DefaultCustomerService struct {
 	repo domain.CustomerRepository
@@ -11,6 +12,10 @@ type DefaultCustomerService struct {
 
 func (s DefaultCustomerService) GetAllCustomer() ([]domain.Customer, error) {
 	return s.repo.FindAll()
+}
+
+func (s DefaultCustomerService) GetCustomer(id string) (*domain.Customer, error) {
+	return s.repo.ById(id)
 }
 
 func NewCustomerService(repository domain.CustomerRepository) DefaultCustomerService {

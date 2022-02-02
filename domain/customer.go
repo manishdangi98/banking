1cd99b9576a3b7b836d9834f41c2445f5eca66ff
Manish Dangi <manish1998dangi@gmail.com>
2022-02-02 17:15:56 +0530

    Data transfer object

--- domain/customer.go
+++ domain/customer.go
@@ -1,6 +1,9 @@
 package domain
 
-import "github.com/manishdangi98/banking/errs"
+import (
+	"github.com/manishdangi98/banking/dto"
+	"github.com/manishdangi98/banking/errs"
+)
 
 type Customer struct {
 	Id          string `db:"customer_id"`
@@ -12,7 +15,25 @@ type Customer struct {
 }
 
 type CustomerRepository interface {
-	//status == 1 status == 0  status == ""
 	FindAll(status string) ([]Customer, *errs.AppError)
 	ById(string) (*Customer, *errs.AppError)
+}
+
+func (c Customer) statusAsText() string {
+	statusAsText := "active"
+	if c.Status == "0" {
+		statusAsText = "inactive"
+	}
+	return statusAsText
+}
+
+func (c Customer) ToDto() dto.CustomerResponse {
+	return dto.CustomerResponse{
+		Id:          c.Id,
+		Name:        c.Name,
+		City:        c.City,
+		Zipcode:     c.Zipcode,
+		DateofBirth: c.DateofBirth,
+		Status:      c.statusAsText(),
+	}
 }

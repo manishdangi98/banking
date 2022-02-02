00f74b3712a15a8e06a3e1fb7ad9d0bee8ac9ac5
Manish Dangi <manish1998dangi@gmail.com>
2022-02-02 11:44:23 +0530

    Added sqlx

--- domain/customerRepositoryDb.go
+++ domain/customerRepositoryDb.go
@@ -5,50 +5,36 @@ import (
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"github.com/manishdangi98/banking/errs"
 	"github.com/manishdangi98/banking/logger"
 )
 
 type CustomerRepositoryDb struct {
-	client *sql.DB
+	client *sqlx.DB
 }
 
 func (d CustomerRepositoryDb) FindAll(status string) ([]Customer, *errs.AppError) {
-	var rows *sql.Rows
 	var err error
+	customers := make([]Customer, 0)
 	if status == "" {
 		findAllSql := "select customer_id, name, city, zipcode, date_of_birth, status from customers"
-		rows, err = d.client.Query(findAllSql)
-
+		err = d.client.Select(&customers, findAllSql)
 	} else {
 		findAllSql := "select customer_id, name, city, zipcode, date_of_birth, status from customers where status = ?"
-		rows, err = d.client.Query(findAllSql, status)
+		err = d.client.Select(&customers, findAllSql, status)
 
 	}
 	if err != nil {
 		logger.Error("Error while quering customer table" + err.Error())
 		return nil, errs.NewUnexpectedError("Unexpected database error")
 	}
-	customers := make([]Customer, 0)
-
-	for rows.Next() {
-		var c Customer
-		err := rows.Scan(&c.Id, &c.Name, &c.City, &c.Zipcode, &c.DateofBirth, &c.Status)
-
-		if err != nil {
-			logger.Error("Error while scanning customer table" + err.Error())
-			return nil, errs.NewUnexpectedError("Unexpected database error")
-		}
-		customers = append(customers, c)
-	}
 	return customers, nil
-
 }
 func (d CustomerRepositoryDb) ById(id string) (*Customer, *errs.AppError) {
 	customerSql := "select customer_id, name, city, zipcode, date_of_birth, status from customers where customer_id = ?"
-	row := d.client.QueryRow(customerSql, id)
 	var c Customer
-	err := row.Scan(&c.Id, &c.Name, &c.City, &c.Zipcode, &c.DateofBirth, &c.Status)
+	err := d.client.Get(&c, customerSql, id)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			return nil, errs.NewNotFoundError("Customer not found")
@@ -61,13 +47,12 @@ func (d CustomerRepositoryDb) ById(id string) (*Customer, *errs.AppError) {
 }
 
 func NewCustomerRepositoryDb() CustomerRepositoryDb {
-	client, err := sql.Open("mysql", "root:@tcp(localhost:3306)/banking")
+	client, err := sqlx.Open("mysql", "root:@tcp(localhost:3306)/banking")
 	if err != nil {
 		panic(err)
 	}
 	client.SetConnMaxIdleTime(time.Minute * 3)
 	client.SetMaxIdleConns(10)
 	client.SetMaxOpenConns(10)
-
 	return CustomerRepositoryDb{client}
 }
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 )
 
 require (
+	github.com/jmoiron/sqlx v1.3.4 // indirect
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.7.0 // indirect
 )

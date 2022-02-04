c9c247e584217dab0fa2c245cf26edc50ef6b7d8
Manish Dangi <manish1998dangi@gmail.com>
2022-02-04 23:31:43 +0530

    Implemented Transaction API

--- domain/account.go
+++ domain/account.go
@@ -6,18 +6,27 @@ import (
 )
 
 type Account struct {
-	AccountId   string
-	CustomerId  string
-	OpeningDate string
-	AccountType string
-	Amount      float64
-	Status      string
-}
-
-type AccountRepository interface {
-	Save(Account) (*Account, *errs.AppError)
+	AccountId   string  `db:"account_id"`
+	CustomerId  string  `db:"customer_id"`
+	OpeningDate string  `db:"opening_date"`
+	AccountType string  `db:"account_type"`
+	Amount      float64 `db:"amount"`
+	Status      string  `db:"status"`
 }
 
 func (a Account) ToNewAccountResponseDto() dto.NewAccountResponse {
 	return dto.NewAccountResponse{a.AccountId}
+}
+
+type AccountRepository interface {
+	Save(Account) (*Account, *errs.AppError)
+	SaveTransaction(transaction Transaction) (*Transaction, *errs.AppError)
+	FindBy(accountId string) (*Account, *errs.AppError)
+}
+
+func (a Account) CanWithdraw(amount float64) bool {
+	if a.Amount < amount {
+		return false
+	}
+	return true
 }

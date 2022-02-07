dc808173e547d46e6b78e729fca96a4964332648
Manish Dangi <manish1998dangi@gmail.com>
2022-02-07 16:47:04 +0530

    Unit test for accountService

--- domain/account.go
+++ domain/account.go
@@ -20,6 +20,7 @@ func (a Account) ToNewAccountResponseDto() *dto.NewAccountResponse {
 	return &dto.NewAccountResponse{a.AccountId}
 }
 
+//go:generate mockgen -destination=../mocks/domain/mockAccountRepository.go -package=domain github.com/manishdangi98/banking/domain AccountRepository
 type AccountRepository interface {
 	Save(Account) (*Account, *errs.AppError)
 	SaveTransaction(transaction Transaction) (*Transaction, *errs.AppError)
